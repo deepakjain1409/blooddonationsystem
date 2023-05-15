2193358e27c7a340eca0de67669125dead46bec8
deepakjain1409 <112298618+deepakjain1409@users.noreply.github.com>
2023-05-15 20:26:49 +0530

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,49 @@
+<?php 
+include 'connect.php'; session_start(); 
+?> 
+<!DOCTYPE html> 
+<html> 
+<head> 
+	 	<meta charset="utf-8"> 
+	 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	 	<title>php project</title> 
+ 
+	 	<link rel="stylesheet" type="text/css" href="style.css"> 
+ 
+	 	<link rel="stylesheet" 
+href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
+    <script 
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>     <script 
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></scri pt> 
+ 
+</head> 
+<body> 
+	 	<header class="header"> 
+	 	 	<br> 
+	 	 	<br> 
+	 	 	<br> 
+	 	 	<div class="container"> 	 	 	 
+	 	 	 	<div class="row"> 
+	 	 	 	 	<div class="col-sm-6 banner-info" > 
+	 	 	 	 	 	<h2><b>Be Responsive</b></h2> 
+ 	 	 	 	 	<p class="big-text">"Donate!"</p>  	 	 	 	 	<p class="big-text">"It is a bloody good job."</p> 
+	 	 	 	 	 	<p>Are u Willing to Donate the Blood for 
+the Needy, Join our Blood Donors family by clicking this blood donors registration link.</p> 
+	 	 	 	 	 	<a class="btn btn-first"  
+href="contact.php">Sign Up</a> 
+	 	 	 	 	 	<a class="btn btn-second"  
+	href="sign_up.php">Sign In</a> 	 	 	 	 
+	 	 	 	 	</div> 
+	 	 	 	 	<div class="col-sm-6 banner-image"> 
+	 	 	 	 	 	<br> 
+	 	 	 	 	 	<br> 
+	 	 	 	 	 	<br> 
+	 	 	 	 	 	<br> 
+	 	 	 	 	 	<img src="sonali.png" class="img-
+	responsive" >  	 
+	 	 	 	 	</div> 
+	 	 	 	</div> 
+	 	 	</div> 
+	 	</header> 
+</body> 
+</html> 
